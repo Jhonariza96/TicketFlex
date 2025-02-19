a61e492aa56f98d071d2906e16f6f5778b897629
Jhon Fredy Ariza Peña <jhonfredy199629@gmail.com>
2025-02-19 02:59:40 -0500

    Update WebConfig.java

--- main/java/com/tu_paquete/ticketflex/WebConfig.java
+++ main/java/com/tu_paquete/ticketflex/WebConfig.java
@@ -1,5 +1,4 @@
 package com.tu_paquete.ticketflex;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
